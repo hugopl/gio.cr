d7a7ac4936cfe1651a2584778ab68dc4635caf8b
Hugo Parente Lima <hugo.pl@gmail.com>
2023-12-04 20:01:13 -0300

    Let Gio::ListModel#items_changed accept any time of Int and convert to UInt32.

--- src/bindings/gio/list_model.cr
+++ src/bindings/gio/list_model.cr
@@ -1,6 +1,6 @@
 module Gio
   module ListModel
-    def items_changed(position : Int32, removed : Int32, added : Int32) : Nil
+    def items_changed(position : Int, removed : Int, added : Int) : Nil
       items_changed(position.to_u32, removed.to_u32, added.to_u32)
     end
   end
